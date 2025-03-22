3b7290d04988f03d1fd2843d7dc5d1ec17b19255
krisnachaitanya <stealgamerofficial@gmail.com>
2025-03-22 17:58:54 +0530

    changed url

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
   useEffect(() => {
     if (!room) return;
     
-    const newSocket = new WebSocket("wss://ballast.proxy.rlwy.net:26842");
+    const newSocket = new WebSocket("ws://ec2-52-66-255-189.ap-south-1.compute.amazonaws.com");
     setSocket(newSocket);
     
     newSocket.onopen = () => {
